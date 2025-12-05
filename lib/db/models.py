a254e7cc369beef4ad97b4eaf6a761698b756423
Brandon2401 <dikirrbrandon@gmail.com>
2025-12-05 13:26:59 +0300

    added the models.py file

---
+++ lib/db/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, DateTime, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import datetime
+
+DATABASE_URL = "sqlite:///notes.db"
+
+Base = declarative_base()
+
+class Note (Base):
+    __tablename__="notes"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    body = Column(String, nullable=False)
+    tags = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"<Note{self.id}:{self.title}>"
+    
+    engine = create_engine(DATABASE_URL)
+    Session = sessionmaker(bind=engine)
+    session = Session()
